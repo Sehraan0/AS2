4bf8b2b7e0468111bd0f337022aa9b058299a9df
Serhaan Saleem <serhaan.saleem21@my.northampton.ac.uk>
2023-07-14 18:08:52 +0100

    character stopped from walking into cactus

--- game.js
+++ game.js
@@ -35,22 +35,48 @@ function move() {
 
 	if (downPressed == true) {
 		var newTop = positionTop + 1;
-		player.style.top = newTop + 'px';
+
+		/* var element added to move function so player cant walk into cactus. SAME CODE FOR ALL MOVEMENTS BELOW WITH TINY CHANGES*/
+		var element = document.elementFromPoint(positionLeft, newTop + 46); /* for down key player heigh is added*/
+
+		if (element.classList.contains('cactus') == false) { /* if statement which targets the cactus class. there is no cactus the player can move*/
+			player.style.top = newTop + 'px';
+		}
 		player.className = 'character walk down';
 	}
+
 	if (upPressed == true) {
 		var newTop = positionTop - 1;
-		player.style.top = newTop + 'px';
+		var element = document.elementFromPoint(positionLeft, newTop); /*height is removed as this requires character to start from top*/
+
+		if (element.classList.contains('cactus') == false) {
+			player.style.top = newTop + 'px';
+
+		}
 		player.className = 'character walk up';
 	}
+
+
 	if (leftPressed == true) {
 		var newLeft = positionLeft - 1;
-		player.style.left = newLeft + 'px';
+
+		var element = document.elementFromPoint(newLeft, positionTop); /* slight change for left and right*/
+
+		if (element.classList.contains('cactus') == false) {
+			player.style.left = newLeft + 'px';
+		}
 		player.className = 'character walk left';
 	}
+
+
 	if (rightPressed == true) {
 		var newLeft = positionLeft + 1;
-		player.style.left = newLeft + 'px';
+
+		var element = document.elementFromPoint(newLeft + 32, positionTop);
+
+		if (element.classList.contains('cactus') == false) {
+			player.style.left = newLeft + 'px';           /* if element always stays the same var element has slight changes, it uses different coordinations*/
+		}
 		player.className = 'character walk right';
 	}
 }
@@ -82,33 +108,39 @@ document.addEventListener('DOMContentLoaded', myLoadFunction);
 /*////////////////////////////////////////////////////////////// MY CODE /////////////////////////////////////////////////////////////*/
 
 function myLoadFunction() {
-	var start = document.getElementsByClassName('start')[0]; /* start button element*/ 
+	var start = document.getElementsByClassName('start')[0]; /* start button element*/
 	start.addEventListener('click', startGame) /* click event for the start button*/
-	
+
 	positionTank();
+
 }
 
 function startGame() {
 	var start = document.getElementsByClassName('start')[0];
 	start.style.display = 'none'; /* css property display to make start button disappear*/
-	
-/* movement code moved from myloadfunction to startgame function so character can only move once start game has been clicked*/
+
+	/* movement code moved from myloadfunction to startgame function so character can only move once start game has been clicked*/
 	timeout = setInterval(move, 10);
 	document.addEventListener('keydown', keydown);
 	document.addEventListener('keyup', keyup);
-} 
+}
 
 /* tanks created at random positions*/
 
 function positionTank() {
 	var tank = document.getElementsByClassName('tank'); /* target all tanks*/
 
-	for(var i = 0; i < tank.length; i++) { /* for looping all tanks */ 
+	for (var i = 0; i < tank.length; i++) { /* for looping all tanks */
 		var random = Math.ceil(Math.random() * 100);
 		tank[i].style.top = random + 'vh';
 
 	}
 }
+
+
+/*            Stop character from walking into the cactus                */
+
+
 
 
 
